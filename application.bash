bc5eb2a9c9e31bed8a336d3092118ac1b84f9608
Jacob John <complex3000@gmail.com>
2016-05-01 12:49:11 -0400

    First Commit

---
+++ application.bash
@@ -0,0 +1,28 @@
+### Directory Details ###
+#/ How this app is structured: /#
+
+|---app # Uncompiled Application Code 
+|---config # Production Configuration Files
+|---public # Client Files
+|---server # Server Code
+|---tests # Server Side Production Specs
+|---application.bash # This File
+|---server.js # Server
+|---package.json
+
+### Technologies / Dependencies ###
+#/ What you need to run this app: /#
+
+Server: node.js ( https://nodejs.org/en/ )
+Build System: webpack ( https://webpack.github.io/ )
+Testing: Jasmine ( http://jasmine.github.io/ )
+Front End Framework: Angular 2 ( https://angular.io/ )
+Linting: TSLint
+
+### Engineering Conventions ###
+#/ Suggested Coding Practices for Interacting with this Project /#
+
+Architecture: Component-based Engineering
+Testing: BDD / TDD
+
+
