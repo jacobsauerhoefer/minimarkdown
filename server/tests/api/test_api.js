c7cd095f28ca1cf334a0e60637da1154d2289649
Jacob John <jacobsauerhoefer@gmail.com>
2016-05-12 17:03:19 -0400

    Refactor: Renaming Tests, updating

--- server/tests/api/test_api.js
+++ server/tests/api/test_api.js
@@ -1,5 +1,5 @@
 var request = require("request");
-// var helloWorld = require("../source/server.js");
+var helloWorld = require("../source/server.js");
 var base_url = "http://localhost:3000/";
 
 describe("Hello World Server", function() {
@@ -11,12 +11,12 @@ describe("Hello World Server", function() {
       });
     });
 
-    // it("returns Hello World", function(done) {
-    //   request.get(base_url, function(error, response, body) {
-    //     expect(body).toBe("<h1>Hello World</h1>");
-    //     helloWorld.closeServer();
-    //     done();
-    //   });
-    // });
+    it("returns Hello World", function(done) {
+      request.get(base_url, function(error, response, body) {
+        expect(body).toBe("<h1>Hello World</h1>");
+        helloWorld.closeServer();
+        done();
+      });
+    });
   });
 });
