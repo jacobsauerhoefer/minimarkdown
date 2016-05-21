9b5ffa01cd4f70fa9a4d3a469644f511d78ff704
Jacob John <jacobsauerhoefer@gmail.com>
2016-05-20 22:34:40 -0400

    refactor: removed jasmine-node (last commit 2014 on github) introduced mocha testing

---
+++ server/tests/test.js
@@ -0,0 +1,38 @@
+var assert = require('assert'),
+    http = require('http');
+
+var server = require('../source/server.js');
+
+describe('server', function () {
+  before(function () {
+    server.listen(8000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+
+  it('should say "Hello, world!"', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        assert.equal('Hello, world!\n', data);
+        done();
+      });
+    });
+  });
+});
